2451386c0d78f942c9c68c781f4977ed23de57e1
FdHerrera <soyfdherrera@gmail.com>
2023-04-04 10:43:16 -0300

    Migrates most setters to lua based config

--- init.vim
+++ init.vim
@@ -30,35 +30,8 @@ Plug 'https://github.com/jose-elias-alvarez/null-ls.nvim'
 Plug 'udalov/kotlin-vim'
 call plug#end()
 
-let mapleader = ","
-
-" Extended lines beyond display (scroll needed)
-set nowrap
-
-" No highlight in Search
-set nohlsearch
-
-" Some servers have issues with backup files, see #649.
-set nobackup
-set nowritebackup
-
-" Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
-" delays and poor user experience.
-set updatetime=300
-
-" Always show the signcolumn, otherwise it would shift the text each time
-" diagnostics appear/become resolved.
-set signcolumn=yes
-
 "Setters
 syntax on
-set guifont=Fira\ Code
-colorscheme terafox
-set nocursorline 
-set rnu nu 
-hi Normal guibg=NONE ctermbg=NONE
-set showmatch
-set termguicolors
 
 lua require('init')
 
--- lua/vim-sets/init.lua
+++ lua/vim-sets/init.lua
@@ -1,3 +1,41 @@
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+local set = vim.opt
+
+-- Tabs
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+
+-- Min Up and Down
+set.scrolloff = 8
+
+-- Indent
+set.smartindent = true
+
+-- No backup
+set.backup = false
+set.writebackup = false
+
+set.signcolumn = 'yes'
+
+-- Current lumber line and relative up and down
+set.nu = true
+set.relativenumber = true
+
+-- Show match search do not highlight previous searchs
+set.showmatch = true
+set.hlsearch = false
+
+-- Writes beyond margin
+set.wrap = false
+
+-- Updates more frequently
+set.updatetime = 50
+
+vim.o.termguicolors = true
+vim.cmd.colorscheme('nightfox')
+set.guifont = 'Fira Code'
+vim.api.nvim_set_hl(0, "Normal", { bg = 'None', ctermbg = 'None' })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = 'None', ctermbg = 'None' })
+
+-- Maps leader
+vim.g.mapleader = ","
