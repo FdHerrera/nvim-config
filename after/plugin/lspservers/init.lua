0d2172de961566efaac6497b1f2609c9bf669537
FdHerrera <soyfdherrera@gmail.com>
2023-04-16 20:15:44 -0300

    Moves servers to lua folder to be requirable outside config

--- after/plugin/lspservers/init.lua
+++
@@ -1,4 +0,0 @@
-require("after.plugin.lspservers.lua-server")
-require("after.plugin.lspservers.vim-server")
-require("after.plugin.lspservers.kotlin-server")
-require("after.plugin.lspservers.vue-server")
---
+++ lua/lspservers/init.lua
@@ -0,0 +1,4 @@
+require("lspservers.lua-server")
+require("lspservers.vim-server")
+require("lspservers.kotlin-server")
+require("lspservers.vue-server")
